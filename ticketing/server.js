ffceffd26122b222acc7e76dddf26f858fb4ab42
umingming <u_0112@naver.com>
2024-01-07 17:15:26 +0900

    click after render

--- ticketing/server.js
+++ ticketing/server.js
@@ -1,71 +1,98 @@
-// 공식문서 예제이다.
 const puppeteer = require('puppeteer');
+//시간 지정(month - 1)
+const targetTime = new Date(2024, 00, 07, 17, 17);
 
 (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+    //로그인
     await page.goto('https://www.kbl.or.kr/auth/login');
     await page.type(".login-box .box li input[type='email']", 'u_0112@naver.com');
     await page.type(".login-box .box li input[type='password']", 'tndnjseo13!');
     await page.keyboard.press('Enter');
-    await page.waitForSelector('.contents.main', {
-        visible: true,
-    });
-    await page.waitForSelector('.ic-ticket-24', {
-        visible: true,
-    });
-    await page.click('.header-wrap .ic-ticket-24');
-    await page.waitForSelector('.reserve-list', {
-        visible: true,
-    });
-    await page.click('.reserve-list li:nth-child(2) button');
+
+    /**
+     * @param {puppeteer.Page} targetPage 
+     * @param {String} selector 
+     * @param {Number} delay ms 
+     */
+    async function clickAfterRender(targetPage, selector, delay = 0) {
+        try {
+            await targetPage.waitForSelector(selector, { visible: true });
+            const target = await targetPage.$(selector);
+            console.log(target, selector, delay);
+            if (!target) {
+                await clickAfterRender(targetPage, selector);
+                return;
+            }
+
+            setTimeout(async () => {
+                await targetPage.click(selector);
+            }, delay)
+        } catch (error) {
+            console.log(error);
+            await clickAfterRender(targetPage, selector);
+        }
+    } 
+
+    //예매 버튼 대기
+    const ticketBtn = ".header-wrap .ic-ticket-24";
+    const remainingTime = Math.max(targetTime.getTime() - Date.now(), 1); 
+    await clickAfterRender(page, ticketBtn, remainingTime);
+
+    //리스트에서 몇 번째 경기인지 선택
+    const popupBtn = ".reserve-list li:nth-child(4) button";
+    await clickAfterRender(page, popupBtn);
     
+    //popUp 페이지
     const popup = await browser.waitForTarget(
         (target) => target.url().includes('https://facility.ticketlink.co.kr/')
     )
     const popupPage = await popup.page()
-    await popupPage.waitForSelector('.first', {
-        visible: true,
-    });
-    await popupPage.click('.first .btn.btn_reserve');
+
+    //첫번째 일정
+    const reserveBtn = ".first .btn.btn_reserve";
+    await clickAfterRender(popupPage, reserveBtn);
+
+    //경고 팝업 나올 경우 대비
     await popupPage.keyboard.press('Enter');
-    await popupPage.waitForSelector('#select_seat_grade li:nth-child(9)', {
-        visible: true,
-    });
-    await popupPage.click('#select_seat_grade li:nth-child(9)');
-    await popupPage.waitForSelector('#select_seat_grade li:nth-child(9) .seat_zone', {
-        visible: true,
-    });
-    await popupPage.click('#select_seat_grade li:nth-child(9) .seat_zone li:nth-child(3)');
-    await popupPage.waitForSelector('#main_view_top > #main_view > canvas:last-child', {
-        visible: true,
-    });
-    const elem = await popupPage.$('#main_view_top > #main_view > canvas:last-child');
-    if (elem) {
+
+    //구역 선택
+    async function selectSection(grade, zone) {
+        const gradeSelector = `#select_seat_grade > li:nth-child(${grade})`;
+        const zoneSelector = `${gradeSelector} > .seat_zone li:nth-child(${zone})`;
+
+        await clickAfterRender(popupPage, gradeSelector);
+        await clickAfterRender(popupPage, zoneSelector);
+    }
+    await selectSection(4, 4);
+
+    //좌석 선택
+    const mapSelector = "#main_view_top > #main_view > canvas:last-child";
+    await popupPage.waitForSelector(mapSelector, { visible: true });
+    const map = await popupPage.$(mapSelector);
+
+    if (map) {
+        //배치도 크기 확인
         const rect = await popupPage.evaluate(el => {
             const { top, left, width, height } = el.getBoundingClientRect();
             return { top, left, width, height };
-        }, elem);
-        async function onclick(rect) {
-            for (let i = rect.left + rect.width / 4; i < rect.width / 2; i++) {
-                for (let j = rect.top + rect.height / 2; j < rect.height; j++) {
+        }, map);
+
+
+        async function selectSeat(rect) {
+            for (let i = rect.left + rect.width / 4; i < rect.width; i+=10) {
+                for (let j = rect.top + rect.height / 2; j < rect.height; j+=10) {
                     await popupPage.mouse.click(i, j);
+                    //선택 완료
                     const seat = await popupPage.$('.reserve_right > .reserve_btn > .btn.btn_full');
-                    if (seat) {
-                        return;
-                    }
+                    if (seat) return;
                 }
             }
         }
-        await onclick(rect);
-        await popupPage.click('.reserve_right > .reserve_btn > .btn.btn_full');
-        // await popupPage.waitForSelector('.selectbox:first-child', {
-        //     visible: true,
-        // });
-        // await popupPage.click('.selectbox:first-child');
-        // await popupPage.waitForSelector('.selectbox:first-child .select_list li:nth-child(2)', {
-        //     visible: true,
-        // });
-        // await popupPage.click('.selectbox:first-child .select_list li:nth-child(2)');
+        await selectSeat(rect);
+
+        //다음 단계 이동
+        // await popupPage.click('.reserve_right > .reserve_btn > .btn.btn_full');
     }
 })();
